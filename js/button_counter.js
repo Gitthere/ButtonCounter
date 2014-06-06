5303f621416b42e7a3bb36c335991236470a901a
Gitthere <ichi4g@gmail.com>
2014-06-05 22:47:41 -1000

    eliminated data line

--- js/button_counter.js
+++ js/button_counter.js
@@ -3,20 +3,18 @@
 var button = document.getElementById('count_but');
 var number = 0;
 
-data = counter.innerHTML;
+  button.onclick = function() {
+    //increases count by 1 for every click
+    counter.innerHTML = ++number;
 
-button.onclick = function() {
-  //increases count by 1 for every click
-  counter.innerHTML = ++number;
+    //MANIPULATE DATA-COUNT IN DOM
+    counter.setAttribute('data-count', number);
 
-  //MANIPULATE DATA-COUNT IN DOM
-  counter.setAttribute('data-count', number);
+    //SET CLASS SIZE IN DOM
+    counter.setAttribute('class', 'size' + number);
 
-  //SET CLASS SIZE IN DOM
-  counter.setAttribute('class', 'size' + number);
-
-  //OUTPUT TO CONSOLE
-  console.log(counter);
-}
+    //OUTPUT TO CONSOLE
+    console.log(counter);
+  }
 })();
 
