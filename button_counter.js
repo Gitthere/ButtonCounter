8751fc36ccd1eebfc1a95262b010822c47953bde
Gitthere <ichi4g@gmail.com>
2014-06-05 14:31:38 -1000

    need to figure out if soln for DOM counter is data attribute or .text use in function after onclick

--- button_counter.js
+++ button_counter.js
@@ -3,6 +3,22 @@ var button = document.getElementById('count_but');
 
 var count = 0;
 var number = 0;
+
+//FIRST OPTION TO CHANGE COUNTER IN DOM
+//use of data attribute to try to
+//get data-count to change, not working
+var div = document.getElementById('div');
+  data = counter.innerHTML;
+  //div.data("count");
+
 button.onclick = function() {
+//   SECOND OPTION
+//   var data = ("data-count").text();
+//   ("data-count").replaceWith("<div data-count='number'/>");
+//   ("data-count").val(data);
+// })
+
   counter.innerHTML = ++number;
+  console.log(counter);
 };
+
